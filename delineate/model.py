7ad8f8485d7a98381fe78b92e427aa6d2f0f9ce7
Jeet Sukumaran <jeetsukumaran@gmail.com>
2018-02-21 18:16:13 -0500

    WIP

--- delineate/model.py
+++ delineate/model.py
@@ -1,4 +1,5 @@
 #! /usr/bin/env python
+# -*- coding: utf-8 -*-
 
 import random
 import math
@@ -60,10 +61,79 @@ class LineageNode(dendropy.Node):
 
     def __init__(self, tree, **kwargs):
         self.tree = tree
+        # The probability that all of the tips that are descendants of this node have
+        # had at least one "good" speciation event between themselves and this
+        # node.
+        self.algvar_x = None
+        # The probability that none of the tips that are descendants of
+        # this node have had a good speciation event separating them from this
+        # node.
+        self.algvar_y = None
+        # Probability that all of the descendants of this node that are member
+        # of the "good species" clade are separated from this node by 0 good
+        # speciation events and all of the descendants of this node that are
+        # not members of the good species clade are separated from this node by
+        # at least 1 good speciation event.
+        self.algvar_z = None
+        # The status of node i with respect to whether its descendants are in
+        # the focal clade or not:
+        #   (a) 1=“ancestor of only members of c”,
+        #   (b) 2=“ancestor of only non-members of c”, and
+        #   (c) 3=“ancestor of both members of c and non-members of c”
+        # where "c" = the clade of candidate good species.
+        self.algvar_s = None
         dendropy.Node.__init__(self, **kwargs)
 
     def edge_factory(self, **kwargs):
         return LineageEdge(tree=self.tree, **kwargs)
+
+    def calc_probability_of_good_species_clade(self):
+        if not self._child_nodes:
+            self.algvar_x = 0.0
+            self.algvar_y = 0.0
+            if self.taxon in taxa:
+                self.algvar_s = 1
+            else
+                self.algvar_s = 2
+        else:
+            assert len(self._child_nodes) == 2
+            ch1, ch2 = self._child_nodes
+            if ch1.algvar_s == ch2.algvar_s:
+                self.algvar_s = ch1.algvar_s
+            else:
+                self.algvar_s = 3
+            if self.algvar_s == 1:
+                # Set: yi = yr(i)yl(i)  (1 - pr(i))   (1 - pl(i))
+                self.algvar_y = ch1.algvar_y * ch2.algvar_y * ch1.edge.probability_of_no_speciation * ch2.edge.probability_of_no_speciation
+                assert self.algvar_y == ch1.algvar_y * ch2.algvar_y * (1 - ch1.edge.probability_of_any_speciation) * (1 - ch2.edge.probability_of_any_speciation) ## debug
+            elif self.algvar_s == 2:
+                ch1.algvar_t = ch1.edge.probability_of_any_speciation + (ch1.edge.probability_of_no_speciation) * ch1.algvar_x
+                assert ch1.algvar_t == ch1.edge.probability_of_any_speciation + (1 - ch1.edge.probability_of_any_speciation) * ch1.algvar_x # debug
+                ch2.algvar_t = ch2.edge.probability_of_any_speciation + (ch2.edge.probability_of_no_speciation) * ch2.algvar_x
+                assert ch2.algvar_t == ch2.edge.probability_of_any_speciation + (1 - ch2.edge.probability_of_any_speciation) * ch2.algvar_x # debug
+            elif self.algvar_s == 3
+                if ch1.algvar_s != 3 and ch2.algvar_s != 3:
+                    if ch1.algvar_s == 1:
+                        assert ch1.aglvar_s != 1
+                        ch_f = ch1
+                        ch_g = ch2
+                    elif ch2.algvar_s2 == 1:
+                        assert ch1.aglvar_s != 1
+                        ch_f = ch2
+                        ch_g = ch1
+                    else:
+                        raise ValueError
+                    ch_g.algvar_t = ch_g.edge.probability_of_any_speciation + ch_g.edge.probability_of_no_speciation * ch_g.algvar_x
+                    ch_f.algvar_t = ch_f.edge.probability_of_no_speciation * ch_f.algvar_y
+                    Z += (ch_f.algvar_y * ch_f.edge.probability_of_any_speciation)
+                    self.algvar_z = ch_f.algvar_t * ch_g.algvar_t
+                elif ch1.algvar_s == 3 and ch2.algvar_s == 3:
+                    raise ValueError
+                else:
+                    assert (ch1.algvar_s == 3 or ch2.algvar_s == 3) and not (ch1.algvar_s == 3 and ch2.algvar_s == 3) # debug
+                    pass
+            else:
+                raise ValueError
 
 class LineageTree(dendropy.Tree):
 
@@ -83,5 +153,22 @@ class LineageTree(dendropy.Tree):
                     rng=self.rng,
                     shape=1.0,
                     scale=0.1)
+        self.encode_bipartitions()
 
+    def probability_of_good_species_clade(self, taxa):
+        ### Prep
+        if not isinstance(taxa, set):
+            taxa = set(taxa)
+        ### Initialization
+        Z = None
+        # for leaf in self.leaf_node_iter():
+        #     leaf.algvar_x = 0.0
+        #     leaf.algvar_y = 0.0
+        #     if leaf.taxon in taxa:
+        #         leaf.algvar_s = 1
+        #     else
+        #         leaf.algvar_s = 2
+        ### Sweep
+        for ndi in tree.postorder_node_iter():
+            ndi.calc_probability_of_good_species_clade()
 
