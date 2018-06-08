4cd1f3abba6ef57b82bfc6acad6411b80c94b82e
Jeet Sukumaran <jeetsukumaran@gmail.com>
2018-06-08 17:08:18 -0400

    Test for constraints (1)

--- tests/test_probabilities.py
+++ tests/test_probabilities.py
@@ -138,7 +138,33 @@ class LineageTreeJointSpeciesProbabilities(unittest.TestCase):
                     tree.split_bitmask_edge_map[split_bitmask].length = br_len
                 for speciation_rate_config in brlen_config["speciation_rate_configurations"]:
                     tree.speciation_completion_rate = speciation_rate_config["speciation_rate"]
-                    for species_configuration in speciation_rate_config["species_configurations"]:
+                    for spconf_idx, species_configuration in enumerate(speciation_rate_config["species_configurations"]):
+                        species_labels=frozenset([frozenset(s) for s in species_configuration["species"]])
+                        expected_probability=species_configuration["probability"]
+                        obs_probability=tree.calc_joint_probability_of_species(species_labels)
+                        self.assertAlmostEqual(expected_probability, obs_probability, 8)
+
+class LineageTreeJointSpeciesProbabilitiesWithConstraints(unittest.TestCase):
+
+    def test_probs_with_constraints_with_new_tree(self):
+        with open(os.path.join(_pathmap.TESTS_DATA_DIR, "joint_probability_of_species.json")) as src:
+            test_ref = json.load(src)
+        for test_tree_set in test_ref:
+            taxon_namespace = dendropy.TaxonNamespace(test_tree_set["taxon_namespace"])
+            for brlen_config in test_tree_set["branch_length_configurations"]:
+                for speciation_rate_config in brlen_config["speciation_rate_configurations"]:
+                    tree = model.LineageTree.get(
+                            data=test_tree_set["tree"],
+                            schema="newick",
+                            taxon_namespace=taxon_namespace,
+                            )
+                    tree.encode_bipartitions()
+                    for split_bitmask, br_len in brlen_config["branch_lengths"].items():
+                        split_bitmask = int(split_bitmask)
+                        assert split_bitmask in tree.split_bitmask_edge_map, split_bitmask
+                        tree.split_bitmask_edge_map[split_bitmask].length = br_len
+                    tree.speciation_completion_rate = speciation_rate_config["speciation_rate"]
+                    for spconf_idx, species_configuration in enumerate(speciation_rate_config["species_configurations"]):
                         species_labels=frozenset([frozenset(s) for s in species_configuration["species"]])
                         expected_probability=species_configuration["probability"]
                         obs_probability=tree.calc_joint_probability_of_species(species_labels)
