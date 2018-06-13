b125a9d1ce9b46a70db997d707af561ce7c6e301
Jeet Sukumaran <jeetsukumaran@gmail.com>
2018-06-13 00:37:56 -0400

    Getting species partition estimator up to speed

--- bin/delineate-estimate-species-partition.py
+++ bin/delineate-estimate-species-partition.py
@@ -58,6 +58,8 @@ def main():
             help="Output additional information about the tree",)
     utility.add_output_options(parser, output_options=output_options)
     args = parser.parse_args()
+    args.format = "json"
+
     tree = model.LineageTree.get(
             path=args.tree_file,
             schema=args.data_format,
@@ -68,20 +70,12 @@ def main():
     if speciation_completion_rate is None:
         raise ValueError("Speciation completion rate must be specified either in configuration file or by command argument '--speciation-completion-rate'")
     tree.speciation_completion_rate = speciation_completion_rate
-    if "species_leafsets" in config:
-        species_constraints = model._Partition.compile_lookup_key( config["species_leafsets"] )
+    if "species_constraints" in config:
+        species_constraints = model._Partition.compile_lookup_key( config["species_constraints"] )
+        tree.set_node_constraints(species_constraints)
     partition_probability_map = tree.calc_label_partition_probability_map()
-    # initial_speciation_rate = config.pop("initial_speciation_rate", 0.01)
-    # min_speciation_rate = config.pop("min_speciation_rate", 1e-8)
-    # max_speciation_rate = config.pop("max_speciation_rate", 2.00)
-    # mle = MaximumLikelihoodEstimator(
-    #         tree=tree,
-    #         species_leafset_labels=species_leafset_labels,
-    #         initial_speciation_rate=initial_speciation_rate,
-    #         min_speciation_rate=min_speciation_rate,
-    #         max_speciation_rate=max_speciation_rate)
-    # speciation_completion_rate_estimate, speciation_completion_rate_estimate_lnl = mle.estimate_speciation_rate()
-    # extra_fields = utility.parse_fieldname_and_value(args.label)
+
+    result_dict = collections.OrderedDict()
     # if not args.no_header_row:
     #     header_row = []
     #     if args.tree_info:
@@ -90,20 +84,38 @@ def main():
     #     header_row.extend(extra_fields)
     #     header_row.append("estSpCompRate")
     #     header_row.append("estSpCompRateLnL")
-    #     if args.intervals:
-    #         header_row.append("ciLow")
-    #         header_row.append("ciHigh")
     #     sys.stdout.write(args.separator.join(header_row))
     #     sys.stdout.write("\n")
-    # row = []
-    # if args.tree_info:
-    #     row.append("{}".format(len(tree.taxon_namespace)))
-    #     tree.calc_node_ages()
-    #     row.append("{}".format(tree.seed_node.age))
-    # for field in extra_fields:
-    #     row.append(extra_fields[field])
-    # row.append("{}".format(speciation_completion_rate_estimate))
-    # row.append("{}".format(speciation_completion_rate_estimate_lnl))
+    row = []
+    if args.tree_info:
+        result_dict["numTips"] = len(tree.taxon_namespace)
+        tree.calc_node_ages()
+        result_dict["rootAge"] = "{}".format(tree.seed_node.age)
+    extra_fields = utility.parse_fieldname_and_value(args.label)
+    if extra_fields:
+        result_dict.update(extra_fields)
+
+    species_partition_info = [(k, list(list(s) for s in k), math.log(partition_probability_map[k])) for k in partition_probability_map]
+    species_partition_info.sort(key=lambda x: x[2], reverse=True)
+    assert species_partition_info[0][2] >= species_partition_info[-1][2]
+    result_dict["numPartitions"] = len(species_partition_info)
+    max_loglikelihood = species_partition_info[0][2]
+    result_dict["lnLMax"] = max_loglikelihood
+    result_dict["partitions"] = []
+    for key_idx, (key, key_as_list, lnL) in enumerate(species_partition_info):
+        p = collections.OrderedDict()
+        p["partition"] = key_as_list
+        p["lnL"] = lnL
+        if lnL + 1.96 >= max_loglikelihood:
+            p["isInConfidenceInterval"] = True
+        else:
+            p["isInConfidenceInterval"] = False
+        # sys.stderr.write("{}, {}, {}\n".format(max_loglikelihood, lnL, p["isInConfidenceInterval"]))
+        result_dict["partitions"].append(p)
+    if args.format == "json-compact":
+        json.dump(result_dict, sys.stdout)
+    else:
+        json.dump(result_dict, sys.stdout, indent=4, separators=(',', ': '))
     # if args.intervals:
     #     ci_low, ci_high = mle.estimate_confidence_interval(
     #         mle_speciation_rate=speciation_completion_rate_estimate,
