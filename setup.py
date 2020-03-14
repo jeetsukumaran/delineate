ba861fbb269d8d145cc35adff1dbb7da25453ffa
Jeet Sukumaran <jeetsukumaran@gmail.com>
2020-03-14 13:26:29 -0700

    Another approach

--- setup.py
+++ setup.py
@@ -15,13 +15,33 @@ def _read(path_components, **kwargs):
             s = src.read()
         return s
 
+def _get_reqs_and_deps():
+    loc = os.path.abspath(os.path.dirname(__file__))
+    with open(loc + '/requirements.txt') as f:
+        requirements = f.read().splitlines()
+    required = []
+    dependency_links = []
+    # do not add to required lines pointing to git repositories
+    EGG_MARK = '#egg='
+    for line in requirements:
+        if not line:
+            continue
+        if line.startswith('-e git:') or line.startswith('-e git+') or \
+                line.startswith('git:') or line.startswith('git+'):
+            if EGG_MARK in line:
+                package_name = line[line.find(EGG_MARK) + len(EGG_MARK):]
+                required.append(package_name)
+                dependency_links.append(line)
+            else:
+                print('Dependency to a git repository should have the format:')
+                print('git+ssh://git@github.com/xxxxx/xxxxxx#egg=package_name')
+        else:
+            required.append(line)
+    return required, dependency_links
+
 project_init = _read(["src", "delineate", "__init__.py"])
 __version__ = re.match(r".*^__version__\s*=\s*['\"](.*?)['\"]\s*$.*", project_init, re.S | re.M).group(1)
 __project__ = re.match(r".*^__project__\s*=\s*['\"](.*?)['\"]\s*$.*", project_init, re.S | re.M).group(1)
-
-requirements = [line.strip()
-                    for line in open('requirements.txt').readlines()
-                    if line.strip() and not line.startswith('#')]
 
 setup(
     name=__project__,
@@ -40,5 +60,6 @@ setup(
     license="LICENSE.txt",
     description="Model-based species delimitation.",
     long_description=_read(["README.txt"]),
-    install_requires=requirements,
+    install_requires=reqs,
+    dependency_links=deps,
 )
